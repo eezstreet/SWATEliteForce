eezstreet/SWATEliteForce

commit 07a6a9dd183ae6233526468ba154b9647d2f46a2

Inertia/ADS animation speed no longer affected by framerate. Simplified how UpdateHandsForRendering gets called

Source/Game/SwatGame/Classes/SwatPlayer.uc
@@ -2034,7 +2034,15 @@ Begin:
 ////////////////////////////////////////////////////////////////////////////////
 
 simulated function Tick(float dTime) {
-  AdjustPlayerMovementSpeed();
+
+    local Hands MyHands;
+
+    AdjustPlayerMovementSpeed();
+    
+    MyHands = GetHands();
+    if ( MyHands != None )
+        MyHands.UpdateHandsForRendering(dTime);
+
 }
 
 

Source/Unreal/Engine/Classes/Equipment/Hands.uc
@@ -58,15 +58,15 @@ simulated function onMessage(Message m)
     // NOTE: we don't check the class of 'm' because we only register to 
     // receive one kind of message (MessagePreRender, done in 
     // PostNetBeginPlay()) 
-    UpdateHandsForRendering();
 } 
 
-simulated function UpdateHandsForRendering()
+simulated function UpdateHandsForRendering(float deltaTime)
 {
     local Pawn OwnerPawn;
     local PlayerController OwnerController;
     local vector TargetLocation;
-	local float AnimationPosition;
+    local float AnimationPosition;
+	local float AnimationPositionChange;
     local vector NewLocation;
     local rotator NewRotation;
     local HandheldEquipmentModel EquippedFirstPersonModel;
@@ -74,6 +74,7 @@ simulated function UpdateHandsForRendering()
     local vector Offset;
     local float ViewInertia;
     local float ADSInertia;
+	local vector Change;
     
     OwnerPawn = Pawn(Owner);
     if (OwnerPawn == None)
@@ -120,6 +121,11 @@ simulated function UpdateHandsForRendering()
 		NewRotation += EquippedItem.GetDefaultRotationOffset();
 		Offset = EquippedItem.GetDefaultLocationOffset();
 	}
+	//scale animation position change based on framerate
+	AnimationPositionChange = AnimationPosition - EquippedItem.GetIronSightAnimationPosition();
+	AnimationPositionChange = AnimationPositionChange * (deltaTime / 0.016667); //scale relative to 60fps
+	AnimationPosition = EquippedItem.GetIronSightAnimationPosition() + AnimationPositionChange;
+	
 	EquippedItem.SetIronSightAnimationPosition(AnimationPosition);
 	//apply progress of iron sight animation
 	Offset += (EquippedItem.GetIronsightsLocationOffset() * AnimationPosition);
@@ -130,6 +136,11 @@ simulated function UpdateHandsForRendering()
 	//interpolate towards our target location. inertia controls how quickly the weapon 
 	//visually responds to our movements
 	NewLocation = (Location * ViewInertia) + (TargetLocation * (1 - ViewInertia));
+	
+	//scale the motion for this frame based on the framerate
+	Change = NewLocation - Location;
+	Change = Change * (deltaTime / 0.016667); //scale relative to 60fps
+	NewLocation = Location + Change;
 	
 	bOwnerNoSee = !OwnerPawn.bRenderHands;
 
@@ -153,42 +164,36 @@ simulated function OnEquipKeyFrame()
 {
 //    log( self$" in Hands::OnEquipKeyFrame()" );
     Pawn(Owner).OnEquipKeyFrame();
-    UpdateHandsForRendering();
 }
 
 simulated function OnUnequipKeyFrame()
 {
 //    log( self$" in Hands::OnUnequipKeyFrame()" );
     Pawn(Owner).OnUnequipKeyFrame();
-    UpdateHandsForRendering();
 }
 
 simulated function OnUseKeyFrame()
 {
 //    log( self$" in Hands::OnUseKeyFrame()" );
     Pawn(Owner).OnUseKeyFrame();
-    UpdateHandsForRendering();
 }
 
 simulated function OnLightstickKeyFrame()
 {
 //    log( self$" in Hands::OnUseKeyFrame()" );
     Pawn(Owner).OnLightstickKeyFrame();
-    UpdateHandsForRendering();
 }
 
 simulated function OnMeleeKeyFrame()
 {
 //    log( self$" in Hands::OnMeleeKeyFrame()" );
     Pawn(Owner).OnMeleeKeyFrame();
-    UpdateHandsForRendering();
 }
 
 simulated function OnReloadKeyFrame()
 {
 //    log( self$" in Hands::OnReloadKeyFrame()" );
     Pawn(Owner).OnReloadKeyFrame();
-    UpdateHandsForRendering();
 }
 
 //hands idle
@@ -196,7 +201,6 @@ event AnimEnd( int Channel )
 {
     if (Level.GetLocalPlayerController().HandsShouldIdle())
         IdleHoldingEquipment();
-		UpdateHandsForRendering();
 }
 
 //activeItem should be non-None and Idle
@@ -273,7 +277,6 @@ function PlayNewAnim(name Sequence, float Rate, float TweenTime)
 {
     if (!IsAnimating() || GetAnimName() != Sequence)
         PlayAnim(Sequence, Rate, TweenTime);
-		UpdateHandsForRendering();
 }
 
 //called by SwatPlayer::SetLowReady() only when low-ready changes
@@ -283,13 +286,11 @@ function SetLowReady(bool bEnable)
         return;
 
     bIsLowReady = bEnable;
-    UpdateHandsForRendering();
 
     if (Level.GetLocalPlayerController().HandsShouldIdle())
     {
         SetNextIdleTweenTime(0.2);  //we're immediately transitioning, so tween
         IdleHoldingEquipment();
-		UpdateHandsForRendering();
     }
 }
 

commit 29a198837eef164b0fce2ed20faad360560055cc

Hands: better method of getting deltaTime, to fix new jitter caused by 07a6a9d

Source/Game/SwatGame/Classes/SwatPlayer.uc
@@ -2034,15 +2034,7 @@ Begin:
 ////////////////////////////////////////////////////////////////////////////////
 
 simulated function Tick(float dTime) {
-
-    local Hands MyHands;
-
     AdjustPlayerMovementSpeed();
-    
-    MyHands = GetHands();
-    if ( MyHands != None )
-        MyHands.UpdateHandsForRendering(dTime);
-
 }
 
 

Source/Unreal/Engine/Classes/Equipment/Hands.uc
@@ -58,9 +58,10 @@ simulated function onMessage(Message m)
     // NOTE: we don't check the class of 'm' because we only register to 
     // receive one kind of message (MessagePreRender, done in 
     // PostNetBeginPlay()) 
-} 
+	UpdateHandsForRendering();
+}
 
-simulated function UpdateHandsForRendering(float deltaTime)
+simulated function UpdateHandsForRendering()
 {
     local Pawn OwnerPawn;
     local PlayerController OwnerController;
@@ -75,6 +76,7 @@ simulated function UpdateHandsForRendering(float deltaTime)
     local float ViewInertia;
     local float ADSInertia;
 	local vector Change;
+	local float deltaTime;
     
     OwnerPawn = Pawn(Owner);
     if (OwnerPawn == None)
@@ -112,6 +114,7 @@ simulated function UpdateHandsForRendering(float deltaTime)
 	
 	//if the player is zooming, add the iron sight offset to the new location
 	OwnerController = PlayerController(OwnerPawn.Controller);
+	deltaTime = OwnerController.LastDeltaTime;
 	if (OwnerController != None && OwnerController.WantsZoom) {
 		AnimationPosition = (AnimationPosition * ADSInertia + 1 * (1 - ADSInertia));
 		NewRotation += EquippedItem.GetIronsightsRotationOffset();
@@ -137,10 +140,12 @@ simulated function UpdateHandsForRendering(float deltaTime)
 	//visually responds to our movements
 	NewLocation = (Location * ViewInertia) + (TargetLocation * (1 - ViewInertia));
 	
-	//scale the motion for this frame based on the framerate
-	Change = NewLocation - Location;
-	Change = Change * (deltaTime / 0.016667); //scale relative to 60fps
-	NewLocation = Location + Change;
+	if (ViewInertia > 0) {
+		//scale the motion for this frame based on the framerate
+		Change = NewLocation - Location;
+		Change = Change * (deltaTime / 0.016667); //scale relative to 60fps
+		NewLocation = Location + Change;
+	}
 	
 	bOwnerNoSee = !OwnerPawn.bRenderHands;
 

Source/Unreal/Engine/Classes/PlayerController.uc
@@ -2810,9 +2810,8 @@ event TravelPostAccept()
 
 event PlayerTick( float DeltaTime )
 {
-#if IG_SWAT
-    LastDeltaTime = DeltaTime;
-#endif
+	LastDeltaTime = DeltaTime;
+
 
 #if !IG_SHARED	// rowan: this is essentially a hack.. we handle this better now by explicitly precaching actors on the client
 	//if ( bForcePrecache && (Level.TimeSeconds > ForcePrecacheTime) )